17aa2d449236405f611aaa823256179fb6d7deb8
Abdulkhakim Abdrakhman <aabdrakh@e1z2r3p35.42.us.org>
2018-09-16 13:45:15 -0700

    p1 - p2 end

--- src2/ft_putstr_fd.c
+++ src2/ft_putstr_fd.c
@@ -1,40 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*   ft_putstr_fd.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aabdrakh <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/09/13 19:28:57 by aabdrakh          #+#    #+#             */
-/*   Updated: 2018/09/16 13:32:22 by aabdrakh         ###   ########.fr       */
+/*   Created: 2018/09/16 13:39:12 by aabdrakh          #+#    #+#             */
+/*   Updated: 2018/09/16 13:41:23 by aabdrakh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../libft.h"
-#include <stdlib.h>
+#include <unistd.h>
 
-char	*ft_itoa(int n)
+void	ft_putstr_fd(char const *s, int fd)
 {
-	char			*fresh;
-	unsigned int	t;
-	int				k;
-	int				s;
+	int	i;
 
-	s = 1;
-	t = n;
-	k = (t < 0) ? 1 : 0;
-	t = (t < 0) ? (-t) : t;
-	while (n /= 10 > 0)
-		s++;
-	fresh = malloc(sizeof(char) * (s + k));
-	if (!fresh)
-		return (NULL);
-	fresh[s + k + 1] = '\0';
-	while (t > 0)
+	i = 0;
+	while (s[i] != '\0')
 	{
-		fresh[s + k] = (t % 10);
-		t /= 10;
-		s--;
+		write(fd, &s[i], 1);
+		i++;
 	}
-	return (fresh);
 }
