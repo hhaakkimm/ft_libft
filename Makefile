6e46d9eae78142f2f0d33f9d3535c402e50368e1
Abdulkhakim Abdrakhman <aabdrakh@e1z3r6p17.42.us.org>
2018-09-22 12:45:34 -0700

    ffff

--- Makefile
+++ Makefile
@@ -12,23 +12,34 @@
 
 NAME = libft.a
 
-SRCS = src/*.c src2/*.c src3/*.c
+SRC = ft_atoi.c ft_lstmap.c ft_putendl.c ft_strdup.c ft_strnequ.c	
+SRC += ft_bzero.c ft_lstnew.c ft_putendl_fd.c ft_strequ.c ft_strnew.c	
+SRC += ft_isalnum.c ft_memalloc.c ft_putnbr.c ft_striter.c ft_strnstr.c		
+SRC += ft_isalpha.c ft_memccpy.c ft_putnbr_fd.c ft_striteri.c ft_strrchr.c	
+SRC += ft_isascii.c ft_memchr.c ft_putstr.c ft_strjoin.c ft_strsplit.c	
+SRC += ft_isdigit.c ft_memcmp.c ft_putstr_fd.c ft_strlcat.c ft_strstr.c	
+SRC += ft_isprint.c ft_memcpy.c ft_strcat.c ft_strlen.c ft_strsub.c	
+SRC += ft_itoa.c ft_memdel.c ft_strchr.c ft_strmap.c ft_strtrim.c	
+SRC += ft_lstadd.c ft_memmove.c ft_strclr.c ft_strmapi.c ft_tolower.c	
+SRC += ft_lstdel.c ft_memset.c ft_strcmp.c ft_strncat.c ft_toupper.c	
+SRC += ft_lstdelone.c ft_putchar.c ft_strcpy.c ft_strncmp.c	
+SRC += ft_lstiter.c ft_putchar_fd.c ft_strdel.c ft_strncpy.c
 
-OBJECTS = *.o
+OBJECTS = .o
 
 INCLUDE = libft.h
 
 all: $(NAME)
 
-$(NAME): $(SRCS) libft.h
-	gcc -c -Wall -Wextra -Werror $(SRCS) -I $(INCLUDE)
-	ar rc $(NAME) $(OBJECTS)
-	ranlib $(NAME)
+$(NAME):
+	@gcc -c -Wall -Wextra -Werror $(SRC) -I $(INCLUDE)
+	@ar rc $(NAME) $(OBJECTS)
+	@ranlib $(NAME)
 
 clean:
-	/bin/rm -f $(OBJECTS)
+	@/bin/rm -f $(OBJECTS)
 
 fclean: clean
-	/bin/rm -f $(NAME)
+	@/bin/rm -f $(NAME)
 
 re: fclean all
---
+++ ft_strsplit.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strsplit.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aabdrakh <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/09/13 19:09:36 by aabdrakh          #+#    #+#             */
+/*   Updated: 2018/09/16 16:26:12 by aabdrakh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../libft.h"
+#include <string.h>
+#include <stdlib.h>
+
+static size_t	ft_count_words(char const *s, char c)
+{
+	size_t	cnt;
+
+	cnt = 0;
+	while (*s)
+	{
+		if (*s != c && (*(s + 1) == c || *(s + 1) == '\0'))
+				cnt++;
+		s++;
+	}
+	return (cnt);
+}
+
+char			**ft_strsplit(char const *s, char c)
+{
+	size_t		i;
+	size_t		j;
+	size_t		words;
+	char		**tab;
+
+	i = 0;
+	if (!s || !c)
+		return ((void *)0);
+	words = ft_count_words(s, c);
+	if (!(tab = (char **)malloc(sizeof(char *) * words)))
+		return (NULL);
+	while (i < words)
+	{
+		while (*s == c && *s)
+			s++;
+		tab[i] = (char *)malloc(sizeof(char) * (10000));
+		j = 0;
+		while (*s != c && *s)
+			tab[i][j++] = *s++;
+		tab[i][j] = '\0';
+		i++;
+	}
+	tab[i] = 0;
+	return (tab);
+}
